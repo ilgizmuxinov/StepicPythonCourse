313e4dd0dea690ede8e3e184ffafa55d09daedc6
ilgizmuxinov <ilgiz.muxinov@gmail.com>
2024-07-27 17:41:26 +0500

    Напишите программу, которая по введённому возрасту пользователя сообщает, к какой возрастной группе он относится.

---
+++ 4._Условный_оператор/4.1_Выбор_из_двух/4.1.8.py
@@ -0,0 +1,9 @@
+age = int(input())
+if age <= 13:
+    print('детство')
+if 24 >= age >= 14:
+    print('молодость')
+if 59 >= age >= 25:
+    print('зрелость')
+if age >= 60:
+    print('старость')
