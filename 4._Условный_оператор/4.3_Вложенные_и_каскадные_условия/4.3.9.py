4556f922ffd64550f8b260b71a9b2f6fd4660284
ilgizmuxinov <ilgiz.muxinov@gmail.com>
2024-07-30 16:53:30 +0500

    На числовой прямой даны два отрезка: [a1,b1] и [a2,b2]. Напишите программу, которая находит их пересечение.

---
+++ 4._Условный_оператор/4.3_Вложенные_и_каскадные_условия/4.3.9.py
@@ -0,0 +1,28 @@
+a1 = int(input())
+b1 = int(input())
+a2 = int(input())
+b2 = int(input())
+a1 < b1
+a2 < b2
+if a2 == b1:
+    print(a2)
+if a1 == b2 and b1 > b2:
+    print(a1)
+elif a2 < b2 < a1:
+    print('пустое множество')
+elif b1 < a2:
+    print('пустое множество')
+elif a1 == a2 and b1 == b2:
+    print(a2, b2)
+elif a1 < a2 < b1 < b2:
+    print(a2, b1)
+elif a1 <= a2 and b1 >= b2:
+    print(a2, b2)
+elif a2 < a1 < b2 <= b1:
+    print(a1, b2)
+elif a1 < a2 < b2 < b1:
+    print(a2, b2)
+elif a1 < b1 < a2 < b2:
+    print(a2, b1)
+elif a2 <= a1 < b1 < b2:
+    print(a1, b1)
